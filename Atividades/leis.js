495caff1f8f4e1935967e3191812fc55d7a37739
Jonas Fortes <jonascforte@hotmail.com>
2019-07-25 10:33:57 -0300

    atv1 falta terminar, aparecer e desaparecer card

---
+++ Atividades/leis.js
@@ -0,0 +1,35 @@
+function vermelho(){
+    const elemetoBody = document.querySelector("body");
+    elemetoBody.style.backgroundColor = 'red';
+}
+
+function verde(){
+    const elemetoBody = document.querySelector("body");
+    elemetoBody.style.backgroundColor = 'green';
+}
+
+function azul(  ){
+    const elemetoBody = document.querySelector("body");
+    elemetoBody.style.backgroundColor = 'blue';
+}
+
+function some(){
+    const elementImg = document.getElementById('img');
+    
+}
+function aparece(){
+    const elementImg = document.getElementById('img');
+
+}
+
+// -----------------------------------------------------------------------
+document.getElementById('btmagico').addEventListener('click', function(){
+    some();
+    alert('ola');
+});
+document.getElementById('btmagico2').addEventListener('click', function(){
+    some();
+    alert('ola');   
+});
+
+
